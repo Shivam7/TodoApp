f83101e9421c6ba7c10c6550b888947ab7d65d38
shivam <shivam15009@gmail.com>
2018-07-29 03:24:44 +0530

    Add getTodo route

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ var {Todo}=require('../models/todo');
 var {User}=require('../models/user');
 var app=express();
 app.use(bodyParser.json())
+
+//post request
 app.post('/todos',(req,res)=>{
    console.log("Post request "+req.body); 
 
@@ -21,6 +23,14 @@ app.post('/todos',(req,res)=>{
    })
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+      res.send({todos});
+    }, (e) => {
+      res.status(400).send(e);
+    })
+  });
+  
 
 app.listen(3000,()=>{
     console.log('Started on port 3000');
@@ -28,7 +38,7 @@ app.listen(3000,()=>{
 
 
 
-
+module.exports={app};
 
 
 
