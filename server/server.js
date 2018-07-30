24f5c489701b18f47f942905e274c63ffc169582
shivam <shivam15009@gmail.com>
2018-07-31 00:14:21 +0530

    Added Get/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express=require('express');
 var bodyParser=require('body-parser');
+const {ObjectID}=require('mongodb');
 
 
 var {mongoose}=require('./db/mongoose');
@@ -30,6 +31,25 @@ app.get('/todos', (req, res) => {
       res.status(400).send(e);
     });
   });
+
+  //Get/todos/1223233 :id id variable 
+  app.get('/todos/:id',(req,res)=>{
+      //req.params retreive the id provided
+     var id=req.params.id;
+     if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+     }
+     Todo.findById(id).then((todo)=>{
+         if(!todo){
+             return  res.status(404).send();
+         }
+         res.send({todo});
+
+     }).catch((e)=>{
+        res.status(404).send();
+     })
+  });
+
 
 
 app.listen(3000,()=>{
