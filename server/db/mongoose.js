d25e205c94a5a7544e8acd7d07e8d856e99d96f1
shivam <shivam15009@gmail.com>
2018-07-31 01:31:03 +0530

    Added DB url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var  mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://shivam sharma:@fiqjxn87@ds111059.mlab.com:11059/shivam'||  'mongodb://localhost:27017/TodoApp');
  
 module.exports={
     mongoose
