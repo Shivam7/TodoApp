6de8114057fc529f461803e98fd416c08ef1e316
shivam <shivam15009@gmail.com>
2018-07-31 01:52:29 +0530

    db url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var  mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://shivam:@fiqjxn87@ds111059.mlab.com:11059/shivam');
+mongoose.connect('mongodb://shivam:abc123@ds111059.mlab.com:11059/shivam');
+
 //mongodb://<dbuser>:<dbpassword>@ds111059.mlab.com:11059/shivam
 //|| 'mongodb://localhost:27017/TodoApp'
 module.exports={
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var {mongoose}=require('./db/mongoose');
 var {Todo}=require('../models/todo');
 var {User}=require('../models/user');
 var app=express();
-const port=mongodb://shivam sharma:@fiqjxn87@ds111059.mlab.com:11059/shivam||3000;
+const port=process.env.PORT||3000;
 
 app.use(bodyParser.json())
 
