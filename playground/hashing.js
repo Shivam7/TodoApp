686f051dcdd4566265810405ed6da03b8d5588c0
shivam <shivam15009@gmail.com>
2018-08-13 02:13:06 +0530

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,3 +1,22 @@
+const jwt=require('jsonwebtoken');
+const bcrypt=require('bcryptjs');
+ var password='!123abc';
+
+  bcrypt.genSalt(10,(err,salt)=>{
+    bcrypt.hash(password,salt,(err,hash)=>{
+    console.log("hashed pass", hash);
+      });
+  });
+
+  var hashedPassword='$2a$10$9P1CXAN43coQttvlHCo7Qus3x7KwwsJXOHoBDDE9IsVfLHpUUaT5G';
+  bcrypt.compare(password,hashedPassword,(err,res)=>{
+      console.log(res);
+  })
+
+
+
+
+
 /* const {SHA256}=require('crypto-js');
 var message='I am user number 3'
 var   hash=SHA256(message).toString();
@@ -20,7 +39,7 @@ else{
 }
 */
 
-const jwt=require('jsonwebtoken');
+/*
 
 var data={
     id:10
@@ -29,4 +48,6 @@ var token=jwt.sign(data,'123abc');
 console.log(token);
 
 var decoded=jwt.verify(token,'123abc');
-console.log((decoded));
+console.log((decoded));*/
+
+
